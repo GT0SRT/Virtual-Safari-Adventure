29027867df2b6b7e0f6da58196079d595be9c4aa
himansh-01 <himanshprajapati2004@gmail.com>
2024-09-16 11:57:47 +0530

    animation set

--- script.js
+++ script.js
@@ -3,4 +3,8 @@
         document.querySelector('.loader').style.display = 'flex';
     },1500)
 // loader js end
+setTimeout(function(){
+    document.querySelector(".loader").style.display = 'none'
+    document.querySelector(".main").style.display = 'flex'
+},2000)
 
