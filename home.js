0a859673dbe39290dc5b3edd56850b0fe7fa617e
GT0SRT <140704003+GT0SRT@users.noreply.github.com>
2024-09-17 19:22:39 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,17 +1,6 @@
 // loader js start
 setTimeout(function(){
     document.querySelector('.loader').style.display = 'none';
-    document.querySelector('#page').style.display = 'flex';
+    document.querySelector('#page').style.display = 'block';
 },1000)
 
-
-// colour changer
-document.addEventListener('scroll', function() {
-  const header = document.querySelector('.header');
-  if (window.scrollY > 100) {
-      header.style.display = 'none';
-  }
-  else{
-    header.style.display = 'flex';
-  }
-});
