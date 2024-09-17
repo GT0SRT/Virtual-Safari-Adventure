3e267b08efb53d7434315c223ee3c17c45998b96
GT0SRT <140704003+GT0SRT@users.noreply.github.com>
2024-09-17 13:57:10 +0530

    about.js

--- about.js
+++ about.js
@@ -2,60 +2,36 @@
     setTimeout(function(){
         document.querySelector('.loader').style.display = 'none';
         document.querySelector('#page').style.display = 'flex';
-    },800)
+    },1000)
 
 
     // colour changer
     document.addEventListener('scroll', function() {
       const header = document.querySelector('.header');
-      if (window.scrollY >100) {
-          header.style.backgroundColor = '#824026bd';
+      if (window.scrollY > 100) {
+          header.style.display = 'none';
       }
       else{
-        header.style.backgroundColor = 'transparent'; 
+        header.style.display = 'flex';
       }
   });
 
-
-
-// about page crsr
-document.querySelector('.cover').addEventListener('mousemove',function(dets){
-    document.querySelector('.crsr').style.left = dets.x+"px";
-    document.querySelector('.crsr').style.top = dets.y+"px";
-})
-
 // team
-let elem = document.querySelectorAll('.circle-spin');
-elem.forEach(function(dets){
-    window.addEventListener('scroll', function() {
-        var value = window.scrollY * 1.2;
-        dets.style.transform = `translatex(-50%) translatey(-50%) rotate(${value}deg)`; 
-    });
-})
+// let elem = document.querySelectorAll('.circle-spin');
+// elem.forEach(function(dets){
+//     window.addEventListener('scroll', function() {
+//         var value = window.scrollY * 1.2;
+//         dets.style.transform = `translatex(-50%) translatey(-50%) rotate(${value}deg)`; 
+//     });
+// })
 
-// on scroll animation
-let lane = document.querySelectorAll('.brdr');
-lane.forEach(function(dets){
-    window.addEventListener('scroll', function() {
-        var value = window.scrollY * 0.7;
-        dets.style.width = `${value}px`;
-        dets.style.marginleft = `${value}px`;
-    });
-})
-
-
-// Faq's
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
+// contact
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  var toastEl = document.getElementById('toastSuccess');
+  var toast = new bootstrap.Toast(toastEl, {
+      delay: 3000 
   });
-}
+  toast.show(); 
+  this.reset();
+});
