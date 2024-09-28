f7e65c3d708dbc240f1022510120561c0941548b
GT0SRT <140704003+GT0SRT@users.noreply.github.com>
2024-09-28 09:02:53 +0530

    Update avifauna.js

--- avifauna.js
+++ avifauna.js
@@ -1,8 +1,20 @@
 // loader js start
 setTimeout(function(){
     document.querySelector('.loader').style.display = 'none';
-    document.querySelector('#page').style.display = 'block';
+    document.querySelector('#page').style.display = 'flex';
 },1000)
+
+
+// colour changer
+document.addEventListener('scroll', function() {
+  const header = document.querySelector('.header');
+  if (window.scrollY > 100) {
+      header.style.display = 'none';
+  }
+  else{
+    header.style.display = 'flex';
+  }
+});
 
 // main animation
 let next = document.querySelector('.next')
@@ -17,3 +29,36 @@ prev.addEventListener('click', function(){
     let items = document.querySelectorAll('.item')
     document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
 })
+
+// SHOW MENU 
+const navToggle = document.getElementById('nav-toggle');
+const navMenu = document.getElementById('nav-menu');
+const navClose = document.getElementById('nav-close');
+
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/* REMOVE MENU MOBILE */
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/* ADD BLUR HEADER */
+const blurHeader = () => {
+    const header = document.getElementById("header")
+    this.scrollY >= 50 ? header.classList.add('scroll-header')
+        : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', blurHeader)
